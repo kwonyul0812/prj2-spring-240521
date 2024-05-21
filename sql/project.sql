a68a782a56b06c34262a4e8dc51bcbb0bf6483f6
kwonyul <kw990812@gmail.com>
2024-05-21 12:35:23 +0900

    입력한 값이 비었을 경우 400 코드 리턴

--- sql/project.sql
+++ sql/project.sql
@@ -9,3 +9,6 @@ CREATE TABLE board
     writer   VARCHAR(100)  NOT NULL,
     inserted DATETIME      NOT NULL DEFAULT NOW()
 );
+
+SELECT *
+FROM board;
--- src/main/java/com/prj2spring240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring240521/service/board/BoardService.java
@@ -2,15 +2,32 @@ package com.prj2spring240521.service.board;
 
 import com.prj2spring240521.domain.board.Board;
 import com.prj2spring240521.mapper.board.BoardMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
+@RequiredArgsConstructor
 public class BoardService {
     private final BoardMapper mapper;
 
     public void add(Board board) {
         mapper.insert(board);
+    }
+
+    public boolean validate(Board board) {
+        if (board.getTitle() == null || board.getTitle().isBlank()) {
+            return false;
+        }
+
+        if (board.getContent() == null || board.getContent().isBlank()) {
+            return false;
+        }
+
+        if (board.getWriter() == null || board.getWriter().isBlank()) {
+            return false;
+        }
+        return true;
     }
 }
