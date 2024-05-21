c0a92b7b6578a663923536fc9aab5847dc23d80a
kwonyul <kw990812@gmail.com>
2024-05-21 10:12:47 +0900

    first commit

---
+++ src/main/java/com/prj2spring240521/Prj2Spring240521Application.java
@@ -0,0 +1,13 @@
+package com.prj2spring240521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2Spring240521Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2Spring240521Application.class, args);
+    }
+
+}
