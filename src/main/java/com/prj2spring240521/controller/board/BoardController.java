242a0627eec604d561b2084a864e310f65f7be8e
kwonyul <kw990812@gmail.com>
2024-05-21 15:29:36 +0900

    게시물 삭제

--- src/main/java/com/prj2spring240521/controller/board/BoardController.java
+++ src/main/java/com/prj2spring240521/controller/board/BoardController.java
@@ -43,4 +43,9 @@ public class BoardController {
 
         return ResponseEntity.ok().body(board);
     }
+
+    @DeleteMapping("{id")
+    public void delete(@PathVariable Integer id) {
+        service.remove(id);
+    }
 }
