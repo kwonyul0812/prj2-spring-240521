2e58f7fb518df840fbaa1bbf4045aa04dafb4788
kwonyul <kw990812@gmail.com>
2024-05-23 14:26:52 +0900

    로그인 처리

--- src/main/java/com/prj2spring240521/controller/member/MemberController.java
+++ src/main/java/com/prj2spring240521/controller/member/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -84,5 +85,14 @@ public class MemberController {
         }
     }
 
+    @PostMapping("token")
+    public ResponseEntity token(@RequestBody Member member) {
+        Map<String, Object> map = service.getToken(member);
+        if (map == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(map);
+    }
 
 }
