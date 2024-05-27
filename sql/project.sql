f7938c3e6d9a7728ecc88a469cf455a25d65d55f
kwonyul <kw990812@gmail.com>
2024-05-27 10:15:52 +0900

    마지막 페이지 번호 구하기

--- sql/project.sql
+++ sql/project.sql
@@ -66,3 +66,8 @@ CREATE TABLE authority
 
 INSERT INTO authority (member_id, name)
 VALUES (18, 'admin');
+
+INSERT INTO board
+    (title, content, member_id)
+SELECT title, content, member_id
+FROM board;
