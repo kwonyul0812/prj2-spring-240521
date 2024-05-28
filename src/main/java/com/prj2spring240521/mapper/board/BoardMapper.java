e9087e2f44d24ec566cbba3dd4752ed3b62b3493
kwonyul <kw990812@gmail.com>
2024-05-28 11:38:21 +0900

    첨부된 이미지 출력

--- src/main/java/com/prj2spring240521/mapper/board/BoardMapper.java
+++ src/main/java/com/prj2spring240521/mapper/board/BoardMapper.java
@@ -117,4 +117,12 @@ public interface BoardMapper {
             VALUES (#{boardId}, #{name})
             """)
     int insertFileName(Integer boardId, String name);
+
+
+    @Select("""
+            SELECT name
+            FROM board_file
+            WHERE board_id = #{boardId}
+            """)
+    List<String> selectFileNameByBoardId(Integer boardId);
 }
