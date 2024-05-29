3af1e637184f117a680bec4366d151e3a5b06dfa
kwonyul <kw990812@gmail.com>
2024-05-29 16:21:06 +0900

    좋아요 정보 응답

--- src/main/java/com/prj2spring240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring240521/service/board/BoardService.java
@@ -190,7 +190,9 @@ public class BoardService {
         return board.getMemberId().equals(Integer.valueOf(authentication.getName()));
     }
 
-    public void like(Map<String, Object> req, Authentication authentication) {
+    public Map<String, Object> like(Map<String, Object> req, Authentication authentication) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("like", false);
         Integer boardId = (Integer) req.get("boardId");
         Integer memberId = Integer.valueOf(authentication.getName());
 
@@ -198,8 +200,13 @@ public class BoardService {
         int count = mapper.deleteLikeByBoardIdAndMemberId(boardId, memberId);
 
         // 안했으면
-        if (count == 1) {
+        if (count == 0) {
             mapper.insertLikeByBoardIdAndMemberId(boardId, memberId);
+            result.put("like", true);
         }
+
+        result.put("count", mapper.selectCountLikeByBoardId(boardId));
+
+        return result;
     }
 }
