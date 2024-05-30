53ce6c4e77296be6be3bd04b95b1cb9cf74a721e
kwonyul <kw990812@gmail.com>
2024-05-30 09:21:46 +0900

    게시물 삭제시 좋아요 먼저 삭제

--- src/main/java/com/prj2spring240521/service/board/BoardService.java
+++ src/main/java/com/prj2spring240521/service/board/BoardService.java
@@ -149,6 +149,9 @@ public class BoardService {
         // board_file
         mapper.deleteFileByBoardId(id);
 
+        // board_like
+        mapper.deleteLikeByBoardId(id);
+
         // board
         mapper.deleteById(id);
     }
